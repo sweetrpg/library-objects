70ad65b524875449dbb46b24e24c5c9556e8285d
Paul Schifferer <paul@schifferers.net>
2021-09-05 18:26:33 -0700

    Update volume attributes.

--- src/sweetrpg_library_model/volume.py
+++ src/sweetrpg_library_model/volume.py
@@ -3,21 +3,26 @@ __author__ = "Paul Schifferer <paul@schifferers.net>"
 """
 """
 
-# from sqlalchemy import Column, Integer, String, ForeignKey
-# from sqlalchemy.orm import declarative_base
-from marshmallow import Schema, fields
+from datetime import datetime
 
-# Base = declarative_base()
 
 class Volume(object):
+      """
+      """
       __tablename__ = 'volumes'
 
-      def __init__(self, name, *args, **kwargs):
-            print(name)
-            print(args)
-            print(kwargs)
+      def __init__(self, name, slug, system, *args, **kwargs):
+            """
+            """
+            now = datetime.utcnow().isoformat()
             self.id = kwargs.get('_id')
             self.name = name
+            self.created_at = kwargs.get('created_at', now)
+            self.updated_at = kwargs.get('updated_at', now)
+            self.deleted_at = kwargs.get('deleted_at')
+            self.system = system
+            self.slug = slug
+            self.isbn = kwargs.get('isbn')
 
       def __repr__(self):
-            return f"<Volume(id={self.id!r}, name={self.name!r})>"
+            return f"<Volume(id={self.id!r}, name={self.name!r}, slug={self.slug}, system={self.system}, isbn={self.isbn}, created_at={self.created_at}, updated_at={self.updated_at}, deleted_at={self.deleted_at})>"
