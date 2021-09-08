2f942fca8d417a93bbb5c1ea6c51bdcce7a3f017
Paul Schifferer <paul@schifferers.net>
2021-09-08 07:24:34 -0700

    Add Author

--- src/sweetrpg_library_model/author.py
+++ src/sweetrpg_library_model/author.py
@@ -3,5 +3,25 @@ __author__ = "Paul Schifferer <paul@schifferers.net>"
 """
 """
 
-def Author(object):
-    __tablename__ = 'authors'
+from datetime import datetime
+
+
+class Author(object):
+      """
+      """
+      __tablename__ = 'authors'
+
+      def __init__(self, name, *args, **kwargs):
+            """
+            Creates a new Author object.
+            :param str name: The name of the author.
+            """
+            now = datetime.utcnow().isoformat()
+            self.id = kwargs.get('_id') or kwargs.get('id')
+            self.name = name
+            self.created_at = kwargs.get('created_at', now)
+            self.updated_at = kwargs.get('updated_at', now)
+            self.deleted_at = kwargs.get('deleted_at')
+
+      def __repr__(self):
+            return f"<Author(id={self.id!r}, name={self.name!r}, created_at={self.created_at}, updated_at={self.updated_at}, deleted_at={self.deleted_at})>"
