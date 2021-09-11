e3bfa73115d2e597f297c5bf19740ae888d794e3
Paul Schifferer <paul@schifferers.net>
2021-09-11 16:33:44 -0700

    Fix initializer

--- src/sweetrpg_library_model/volume.py
+++ src/sweetrpg_library_model/volume.py
@@ -11,22 +11,22 @@ class Volume(object):
       """
       __tablename__ = 'volumes'
 
-      def __init__(self, name, slug, system, *args, **kwargs):
+      def __init__(self, *args, **kwargs):
             """
             """
             now = datetime.utcnow().isoformat()
             self.id = kwargs.get('_id') or kwargs.get('id')
-            self.name = name
+            self.name = kwargs['name']
             self.created_at = kwargs.get('created_at', now)
             self.updated_at = kwargs.get('updated_at', now)
             self.deleted_at = kwargs.get('deleted_at')
-            self.system = system
-            self.slug = slug
+            self.system = kwargs['system']
+            self.slug = kwargs['slug']
             self.isbn = kwargs.get('isbn')
-            self.authors = kwargs.get('authors')
-            self.publishers = kwargs.get('publishers')
-            self.studios = kwargs.get('studios')
-            self.reviews = kwargs.get('reviews')
+            self.authors = kwargs.get('authors', [])
+            self.publishers = kwargs.get('publishers', [])
+            self.studios = kwargs.get('studios', [])
+            self.reviews = kwargs.get('reviews', [])
 
       def __repr__(self):
             return f"""<Volume(id={self.id!r},
