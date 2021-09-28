84e076605bbf03b3553008bef0b7a01e6fd4cbee
Paul Schifferer <paul@schifferers.net>
2021-09-28 06:27:23 -0700

    Quoting and formatting

--- src/sweetrpg_library_model/db/volume/document.py
+++ src/sweetrpg_library_model/db/volume/document.py
@@ -14,18 +14,16 @@ class VolumePropertyDocument(EmbeddedDocument):
     """A mapping object for volume properties."""
 
     meta = {
-        'indexes' : [
-            {"name": "volume_property_name", "fields":["name"]}
-        ],
-        'db_alias' : "default",
-        'collection' : "volume_properties",
-        'strict': False,
+        "indexes": [{"name": "volume_property_name", "fields": ["name"]}],
+        "db_alias": "default",
+        "collection": "volume_properties",
+        "strict": False,
     }
 
     name = fields.StringField(min_length=1, max_length=50, required=True)
     value = fields.StringField(min_length=2, max_length=200, required=True)
     kind = fields.StringField(min_length=1, max_length=20, required=True)
-    volume = fields.ReferenceField('VolumeDocument', required=True)
+    volume = fields.ReferenceField("VolumeDocument", required=True)
     created_at = fields.DateTimeField(default=datetime.utcnow, required=True)
     updated_at = fields.DateTimeField(default=datetime.utcnow, required=True)
     deleted_at = fields.DateTimeField(null=True)
@@ -35,20 +33,21 @@ class VolumeDocument(Document):
     """A mapping object to convert MongoDB data to a Volume object."""
 
     meta = {
-        'indexes': [
-            {"name": "volume_slug", "fields": ["slug"], "unique":True},
+        "indexes": [
+            {"name": "volume_slug", "fields": ["slug"], "unique": True},
             {"name": "volume_name", "fields": ["name"]},
             {"name": "volume_system", "fields": ["system"]},
         ],
-        'db_alias': "default",
-        'collection':"volumes",
-        'strict' : False,
+        "db_alias": "default",
+        "collection": "volumes",
+        "strict": False,
     }
 
     name = fields.StringField(min_length=1, max_length=200, required=True)
+    slug = fields.StringField(min_length=2, max_length=50, required=True)
     system = fields.StringField(min_length=1, max_length=20, required=True)
     created_at = fields.DateTimeField(default=datetime.utcnow, required=True)
     updated_at = fields.DateTimeField(default=datetime.utcnow, required=True)
     deleted_at = fields.DateTimeField(null=True)
-    authors = fields.ListField(field=fields.ReferenceField('AuthorDocument'))
+    authors = fields.ListField(field=fields.ReferenceField("AuthorDocument"))
     properties = fields.ListField(fields.EmbeddedDocumentField(VolumePropertyDocument))
