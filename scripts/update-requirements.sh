224498968fb74f549c82c58b1c5bc5568025dadd
Paul Schifferer <paul@schifferers.net>
2021-10-26 10:00:43 -0700

    Updates

--- scripts/update-requirements.sh
+++ scripts/update-requirements.sh
@@ -6,8 +6,9 @@ scriptdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 pushd ${scriptdir}/..
 
-for d in dev docs pkg tests; do
-    pip-compile requirements/$d.in
+for r in pkg docs tests dev; do
+    echo "Requirement: $r"
+    pip-compile requirements/$r.in
 done
 
 popd
--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     install_requires=[
         "Flask==2.0.2",
         "mongoengine",
-        "sweetrpg-db",
-        "sweetrpg-model-core",
+        "sweetrpg-db @ git+https://github.com/sweetrpg/db.git@develop",
+        "sweetrpg-model-core @ git+https://github.com/sweetrpg/model-core.git@develop",
         "marshmallow-jsonapi",
     ],
 )
